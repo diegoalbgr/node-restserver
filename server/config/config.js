0fd8be21c55bfa35d689c19abf336d9f18fcb2dd
Diego <diegoalbgr@hotmail.com>
2020-05-20 14:00:55 -0500

    Variable de entorno MONGO SECURITY

--- server/config/config.js
+++ server/config/config.js
@@ -19,8 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB =
-    "mongodb+srv://strider:x04YFVL1rNJzVdfL@cluster0-y43t0.mongodb.net/cafe";
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
