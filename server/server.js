8a75e5f0adece5220ed6c36eafe2ab8ccdde6900
Diego <diegoalbgr@hotmail.com>
2020-05-20 13:46:18 -0500

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require("./config/config.js");
 
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
 const bodyParser = require("body-parser");
@@ -10,39 +12,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
-  res.json("Hello World");
-});
+app.use(require("./routes/usuario"));
 
-app.get("/usuario", function (req, res) {
-  res.json("get usuario");
-});
-
-app.post("/usuario", function (req, res) {
-  let body = req.body;
-
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      mensaje: "El nombre es necesario",
-    });
-  } else {
-    res.json({
-      persona: body,
-    });
-  }
-});
-
-app.put("/usuario/:id", function (req, res) {
-  let id = req.params.id;
-
-  res.json({
-    id,
-  });
-});
-
-app.delete("/usuario", function (req, res) {
-  res.json("delete usuario");
+mongoose.connect(process.env.URLDB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 app.listen(process.env.PORT, () => {
